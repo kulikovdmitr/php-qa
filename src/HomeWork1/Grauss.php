349f08d400bfd3c609de4b0f83f5ebaa685ea48a
Dmitriy Kulikov <dvkulikov@avito.ru>
2019-11-10 21:27:35 +0300

    Поправил задачки и тесты, сделал без статики

--- src/HomeWork1/Grauss.php
+++ src/HomeWork1/Grauss.php
@@ -7,11 +7,19 @@
 
 class Grauss
 {
-    public static $arr_sum;
-    public function get($start,$stop)
+    public $arr_sum,$stop;
+    public function get()
     {
-        $arr = range($start,$stop);
-        self::$arr_sum = array_sum($arr);
-        echo "Сумма элементов числа равна " . self::$arr_sum;
+        $start = 0;
+        if(isset($this->stop)){
+            if(is_int($this->stop)){
+                $this->arr_sum = array_sum(range($start,$this->stop));
+                echo "Сумма последовательности числа равна " . $this->arr_sum;
+            }else{
+                echo "Введенное число - не является типом INT.";
+            }
+        }else{
+            echo "Введенное число - не задано.";
+        }
     }
 }
--- tests/GraussTest.php
+++ tests/GraussTest.php
@@ -8,21 +8,35 @@ class GraussTest extends TestCase
     {
         require  '../src/Homework1/Grauss.php';
         $grauss = new Grauss();
-        $grauss->get('0',100);
-        self::assertEquals('5050',Grauss::$arr_sum, 'Ожидаемый результат не равен действительному');
+        $grauss->stop = 100;
+        $grauss->get();
+        self::assertEquals(5050,$grauss->arr_sum, 'Ожидаемый результат не равен действительному');
+        unset($grauss->stop);
     }
 
     public function testTwo()
     {
         $grauss = new Grauss();
-        $grauss->get('0',0);
-        self::assertEquals('0',Grauss::$arr_sum, 'Ожидаемый результат не равен действительному');
+        $grauss->stop = 10;
+        $grauss->get();
+        self::assertEquals(55,$grauss->arr_sum, 'Ожидаемый результат не равен действительному');
+        unset($grauss->stop);
     }
 
     public function testThree()
     {
         $grauss = new Grauss();
-        $grauss->get('','');
-        self::assertEquals('0',Grauss::$arr_sum, 'Ожидаемый результат не равен действительному');
+        $grauss->get();
+        self::assertEquals(NULL,$grauss->arr_sum, 'Ожидаемый результат не равен действительному');
+        unset($grauss->stop);
+    }
+
+    public function testFour()
+    {
+        $grauss = new Grauss();
+        $grauss->stop = 'asdf';
+        $grauss->get();
+        self::assertEquals(NULL,$grauss->arr_sum, 'Ожидаемый результат не равен действительному');
+        unset($grauss->stop);
     }
 }
