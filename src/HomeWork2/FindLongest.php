63bbdac68cd808104062df679f4b6543d885a557
Dmitriy Kulikov <dvkulikov@avito.ru>
2019-11-18 21:35:57 +0300

    Черновичок

---
+++ src/HomeWork2/FindLongest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dvkulikov
+ * Date: 18/11/2019
+ * Time: 21:03
+ */
+class FindLongest
+{
+    public function findLongester(string $string)
+    {
+        $arr = array_filter(explode(' ',$string));
+
+        foreach ($arr as $key => $value){
+
+        }
+    }
+}
+
+$result = new FindLongest();
+$result->findLongester('wefhui rgioj 34oijfgh3oi4gj3oi4gj');
