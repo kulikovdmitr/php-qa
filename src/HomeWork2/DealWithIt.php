512757b6aaedec30375798cae2763f76a72719bc
Dmitrij Kulikov <dvkulikov@avito.ru>
2019-11-29 17:57:34 +0300

    DealWithIt

--- src/HomeWork2/DealWithIt.php
+++ src/HomeWork2/DealWithIt.php
@@ -1,40 +1,39 @@
 <?php
-
 class DealWithIt
 {
-    public function findMatchesWithRegexp(string $string)
+    public function findMatchesWithRegexp(string $string):string
     {
         $start = microtime(true);
         $reg_email='/[a-zA-z0-9]{1,64}@[a-zA-z0-9.]{1,255}/';
-        preg_match_all($reg_email,$string,$matches);
-
-        foreach ($matches as $value){
-            foreach ($value as $values){
-                $n = strlen($values);
-                $string = preg_replace($reg_email, str_repeat("*", $n), $string);
-                return $string;
-
-            };
-        }
-        $end = microtime(true);
-        echo "Время выполнения скрипта: ".($end - $start); //вывод результат
-    }
-
-    public function findMatchesWithoutRegexp($string)
-    {
-        $start = microtime(true);
-
-        $arr = explode(" ", $string);
-
-        foreach ($arr as $value) {
-            if (strstr($value, '@') == true) {
-                $value = [$value, strlen($value)];
-                $string = str_replace($value[0],str_repeat("*",intval($value[1])),$string);
-                return $string;
+        preg_match_all($reg_email,$string,$arr);
+        foreach ($arr as $value){
+            foreach ($value as $val){
+                $val_arr = [$val, strlen($val)];
+                $string = str_replace($val_arr[0],str_repeat("*",intval($val_arr[1])),$string);
             }
         }
-
         $end = microtime(true);
-        echo "Время выполнения скрипта: ".($end - $start); //вывод результат
+        echo "Время выполнения скрипта: ".($end - $start);
+        return $string;
+    }
+    public function findMatchesWithoutRegexp(string $string)
+    {
+        $start = microtime(true);
+        $arr = explode(" ", $string);
+        foreach ($arr as $value) {
+            if (strstr($value, '@') != true) {
+                continue;
+            }
+            $firstToken = strtok($value,'@');
+            $secondToken = strtok('@');
+            if($firstToken == null || $secondToken == null){
+                continue;
+            }
+            $value = [$value, strlen($value)];
+            $string = str_replace($value[0],str_repeat("*",intval($value[1])),$string);
+        }
+        $end = microtime(true);
+        echo "Время выполнения скрипта: ".($end - $start);
+        return $string;
     }
 }
